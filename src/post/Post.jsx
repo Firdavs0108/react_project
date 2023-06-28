28e8ac1a4a885ea7db2bc223182ecc1fa9fb833a
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-06-28 23:38:02 +0900

    React_Blog

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Container,Img,PostInfo, PostCats, PostCatsInfo,PostTitle, PostDate } from './style'
+import {Container,Img,PostInfo, PostCats, PostCatsInfo,PostTitle, PostDate, PostDesc } from './style'
 import img4 from '../assets/img/img4.jpg'
 
 const Post = () => {
@@ -15,15 +15,17 @@ const Post = () => {
             </PostCats>
 
             <PostTitle>
-                Lorem ipsum dolor sit amet consectetur 
+                Welcome To Samarkand 
             </PostTitle>
             <hr />
             <PostDate>1 hour ago</PostDate>
-
-
-
-
         </PostInfo>
+                <PostDesc>Samarkand or Samarqand (/ˈsæmərkænd/ SAM-ər-kand; 
+                    Uzbek: Самарқанд, romanized: Samarqand, pronounced 
+                    [sæmærqænd, -ænt]; Tajik: Самарқанд; Persian: سمرقند; 
+                    Sogdian: *smā́rkąθ,smʾrknδH) is a city in southeastern Uzbekistan and among the oldest continuously inhabited cities in Central Asia. There is evidence of human activity in the area of the city from the late Paleolithic Era. Though there is no direct evidence of when Samarkand was founded, several theories propose that it was founded between the 8th and 7th centuries BCE. Prospering from its location on the Silk Road between China, Persia and Europe, at times Samarkand 
+                    was one of the largest[2] cities of Central Asia.[3</PostDesc>
+
     </Container>
   )
 }
--- src/post/style.js
+++ src/post/style.js
@@ -3,24 +3,31 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
-
-  
-
+width: 385px;
+margin: 0px 25px 40px 25px;
 `
 
 export const Img = styled.img`
-
-  
+width: 100%;
+height: 280px;
+object-fit: cover;
+border-radius: 7px;
 `
 
 export const PostInfo = styled.div`
-
-  
+display: flex;
+flex-direction: column;
+align-items: center; 
 `
 
 export const PostCats = styled.div`
-
-  
+font-family: 'Varela Round', sans-serif;
+font-size: 11px;
+color: #be9656;
+line-height: 20px;
+margin-top: 15px;
+margin-right: 10px;
+cursor: pointer;
 `
 export const PostCatsInfo = styled.span`
 
@@ -28,12 +35,35 @@ export const PostCatsInfo = styled.span`
 `
 
 export const PostTitle = styled.span`
+font-family: 'Josefin Sans', sans-serif;
+font-size: 24px;
+font-weight: 700;
+margin-top: 15px;
+cursor: pointer;
+text-align: center;
 
-  
 `
 
 export const PostDate = styled.span`
 
+font-family: 'Lora', serif;
+font-style: italic;
+font-size: 13px;
+color: #999;
+margin-top: 15px;
   
+`
+
+export const PostDesc = styled.p`
+font-family: 'Varela Round', sans-serif;
+font-size: 14px;
+color: #444;
+line-height: 24px;
+margin-top: 15px;
+overflow: hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 4;
+-webkit-box-orient: vertical;
 `
 
