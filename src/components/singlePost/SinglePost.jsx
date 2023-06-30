c3d15e26b007da54a5dded3abda18bbef4fa13d9
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-06-30 23:32:23 +0900

    React_Blog

---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Container} from './style'
+
+
+const SinglePost = () => {
+  return (
+    <Container>
+        single
+        
+    </Container>
+  )
+}
+
+export default SinglePost
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Container} from './style'
 import Sidebar from '../../components/sidebar/Sidebar'
+import SinglePost from '../../components/singlePost/SinglePost'
 
 
 const Single = () => {
   return (
     <Container>
         {/* post */}
+        <SinglePost/>
         <Sidebar/>
     </Container>
   )
