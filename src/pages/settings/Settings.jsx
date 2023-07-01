7d46119914a9da8019a4106a6bd2b8c934717057
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-01 22:48:10 +0900

    React_Blog

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-import {Container} from './style'
+import {Container, SettingTitle, SettingUpTitle, Img, Wrapper,SettingDelTitle, SettingsForm, SettingsPP, SettingsSubmit, SettingsLabel,Forms} from './style'
+import Sidebar from '../../components/sidebar/Sidebar'
+import img4 from '../../assets/img/img4.jpg'
+
 
 const Settings = () => {
   return (
     <Container>
-        ssss
+        <Wrapper>  
+
+          <SettingTitle>
+            <SettingUpTitle>Update Your Account</SettingUpTitle>
+            <SettingDelTitle>Delete Account</SettingDelTitle>
+          </SettingTitle>
+
+          <SettingsForm>
+            <SettingsLabel> Profile Picture</SettingsLabel>
+            <SettingsPP>
+              <Img src={img4} alt="" />
+              <SettingsLabel htmlFor="fileInput">
+              <i class="fa-solid fa-user" style={{width: '25px', height: '25px', borderRadius:'50%', backgroundColor:'lightcoral',
+               color:'white', display:'flex', alignItems:'center', justifyContent:'center', marginLeft:'10px', cursor:'pointer'}}></i>
+              </SettingsLabel>
+              <Forms type="file" id='fileInput' style={{display: 'none'}} />
+            </SettingsPP>
+
+              <SettingsLabel>Username</SettingsLabel>
+              <Forms type="text" placeholder='FredRick' />
+              <SettingsLabel>Email</SettingsLabel>
+              <Forms type="email" placeholder='fredrick@gamil.com' />
+              <SettingsLabel>Password</SettingsLabel>
+              <Forms type="password" />
+              <SettingsSubmit>Update</SettingsSubmit>
+
+          </SettingsForm>
+
+
+        </Wrapper>
+        
+        <Sidebar/>
     </Container>
   )
 }
--- src/pages/settings/style.js
+++ src/pages/settings/style.js
@@ -3,6 +3,68 @@ import styled from "styled-components";
 
 
 export const Container = styled.div`
+    display: flex;
+`
+
+export const Wrapper = styled.div`
+    flex:9;
+    padding:20px;
+`
+
+export const SettingTitle = styled.div`
+    display: flex;
+    align-items:center;
+    justify-content: space-between;
+
+`
+export const SettingUpTitle = styled.span`
+    font-size: 30px;
+    margin-bottom: 20px;
+    color: lightcoral;
+
+`
+
+export const SettingDelTitle = styled.span`
+    color: red;
+    font-style: 12px;
+    cursor: pointer;
+`
+
+export const SettingsForm = styled.form`
+    display: flex;
+    flex-direction: column;
+
+`
+
+export const SettingsPP = styled.div`
+     display: flex;
+     align-items:center;
+     margin: 10px 0;
+
+`
+
+export const Img = styled.img`
+    width: 70px;
+    height: 70px;
+    border-radius: 20px;
+    object-fit: cover;
+`
+
+export const SettingsLabel = styled.label`
+    font-size: 20px;
+    margin-top: 20px;
     
+`
+
+export const Forms = styled.input`
+    color: gray;
+    margin: 10px 0;
+    height: 30px;
+    border: none;
+    border-bottom: 1px solid lightgray;
+    
+`
+
+export const SettingsSubmit = styled.button`
     
 `
