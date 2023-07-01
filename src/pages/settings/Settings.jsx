4187eb98c1a3832792de9e791047f533708457b9
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-01 21:45:08 +0900

    React_Blog

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Container} from './style'
+
+const Settings = () => {
+  return (
+    <Container>
+        ssss
+    </Container>
+  )
+}
+
+export default Settings
