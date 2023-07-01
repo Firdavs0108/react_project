5c5f52f2951926495e49f7557de56c3c9c08c546
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-01 23:10:13 +0900

    React_Blog

--- src/pages/save/Save.jsx
+++ src/pages/save/Save.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Container, LoginForm } from './style'
 
 const Save = () => {
   return (
-    <div>Save</div>
+    <Container>
+        <LoginForm>
+            <label>Email</label>
+            <input type="text" placeholder='Enter Your Email..' />
+            <label>Password</label>
+            <input type="text" placeholder=' Enter Your Password..' />
+        </LoginForm>
+    </Container>
   )
 }
 
